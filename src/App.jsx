fe8584a9f2fe5e99077174af063f1b234ffacc3f
QureshiAwais <awaisqureshi26765@gmail.com>
2026-05-03 12:54:56 +0530

    updated README

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useCallback, useEffect, useRef } from "react";
+
+const App = () => {
+  const [length, setlength] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(false);
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [Password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    // useCallback is a Hook in react that helps you memoize (cache)
+    //  a function so it doesn't get recreated on every render unless its dependencies change
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "1234567890";
+    if (charAllowed) str += "!?@&%?#$£_";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [numberAllowed, charAllowed, length, setPassword]);
+
+  //useEffect [useEffect = "Run this code when something changes"]
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+  // useRef
+  const copyPasswordToClip = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(Password);
+  }, [Password]);
+  const passwordRef = useRef(null);
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto text-center shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white">Password generator</h1>
+        <div className="flex shadow rounded-lg bg-white text-gray-700 overflow-hidden mt-1 mb-4">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClip}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setlength(e.target.value);
+              }}
+            />
+            <label>length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="NumberInput"
+              onChange={() => {
+                setnumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="NumberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="CharacterInput"
+              onChange={() => {
+                // prev this for callback fire
+                setcharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="CharacterInput">characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
